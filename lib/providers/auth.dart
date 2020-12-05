c99861712ec680d783c4639b89b226a87ad9c989
DeleMike <akindelemichael65@gmail.com>
2020-12-05 21:24:12 +0100

    add auto login function

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
@@ -9,6 +10,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -50,9 +52,9 @@ class Auth with ChangeNotifier {
       }
       _token = responseData['idToken'];
       _userId = responseData['localId'];
-      print('User id: $_userId');
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
+          _autoLogout();
       notifyListeners();
     } catch (error) {
       throw (error);
@@ -71,6 +73,18 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) _authTimer.cancel();
+
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    print('Time to expiry: $timeToExpiry');
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
